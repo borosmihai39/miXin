d0bfacfd36a0b2fbcdb317e177d14a0ad6aa2674
borosmihai39 <borosmihai39@gmail.com>
2019-11-21 15:58:05 +0200

    test commit

---
+++ mixin-newapp/src/components/button/buttonComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './buttonComponent.css'
+function ButtonComponent(props) {
+    
+    return (
+        <button onClick={props.methodToRun}>{props.name}</button>
+    )
+}
+
+export default ButtonComponent
